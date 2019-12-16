b2a3d4a20ca0cf2ffced270922e089ce2c11a431
Antoine POUCHET <apouchet@e2r11p22.42.fr>
2019-12-16 17:24:25 +0100

    ok

--- new.py
+++ new.py
@@ -3,9 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from time import sleep
 
+class ClassName(object):
+	
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
+
 class linearRegression():
-	def __init__(self):
-		self.__data = pd.read_csv('data2.csv')
+	"""Class linear regression : call training to train a model"""
+	def __init__(self, path = 'data.csv'):
+		self.__data = pd.read_csv(path)
 		self.setTheta()
 
 	def setTheta(self, const = 0, slope = 0):
@@ -23,6 +31,22 @@ class linearRegression():
 	def estimatePrice(self, mileage):
 		return (self.__slope * mileage + self.__const)
 
+	def display_value(self, mileage):
+		X = self.__data.iloc[:, 0]
+		Y = self.__data.iloc[:, 1]
+		
+		plt.rcParams['figure.figsize'] = (12.0, 9.0)
+		plt.xlabel(sorted(self.__data)[0])
+		plt.ylabel(sorted(self.__data)[1])
+
+		plt.scatter(X, Y, c = "blue")
+		plt.scatter(mileage, self.estimatePrice(mileage), c = "green")
+		print("estimate price for {} = {}".format(mileage, self.estimatePrice(mileage)))
+		
+		plt.show()
+		plt.clf()
+
+
 	def display(self, training = False, step = 1):
 		X = self.__data.iloc[:, 0]
 		Y = self.__data.iloc[:, 1]
@@ -38,9 +62,11 @@ class linearRegression():
 		if training == True:
 			for i in range(len(self.__evol_slope)):
 				if i == 0:
-					plt.plot([min(X), max(X)], [self.__evol_slope[i] * min(X) + self.__evol_const[i], self.__evol_slope[i] * max(X) + self.__evol_const[i]], color='orange')
+					plt.plot([min(X), max(X)], [self.__evol_slope[i] * min(X) + self.__evol_const[i],
+						self.__evol_slope[i] * max(X) + self.__evol_const[i]], color='orange')
 				elif i % step == 0:
-					plt.plot([min(X), max(X)], [self.__evol_slope[i] * min(X) + self.__evol_const[i], self.__evol_slope[i] * max(X) + self.__evol_const[i]], color='green')
+					plt.plot([min(X), max(X)], [self.__evol_slope[i] * min(X) + self.__evol_const[i],
+						self.__evol_slope[i] * max(X) + self.__evol_const[i]], color='green')
 		plt.scatter(X, Y, c = "blue")
 
 		plt.plot([min(X), max(X)], [self.estimatePrice(min(X)), self.estimatePrice(max(X))], color='red')
@@ -48,12 +74,11 @@ class linearRegression():
 		plt.clf()
 
 	def data_info(self):
-		print(self.__data.iloc[0:])
-		print(len(self.__data))
-		# return self.__data
+		return self.__data
 
 
-	def training(self, info = False, learning_slope = 0, learning_const = 0, max_step = 0, precision = 0.5):
+	def training(self, info = False, learning_slope = 0, learning_const = 0, iteration = 0, precision = 0.05):
+		""">> Use training to train your model with the data set in init\n\tArgs : info, learning_slope, learning_const, iteration, precision"""
 		print("start Training")
 		self.setTheta()
 		self.__evol_const = []
@@ -64,8 +89,8 @@ class linearRegression():
 		const = 0
 		step = 0
 
-		if max_step < 0:
-			print("Error : max_step need to be positive")
+		if iteration < 0:
+			print("Error : iteration need to be positive")
 			return
 
 		X = self.__data.iloc[:, 0]
@@ -77,13 +102,12 @@ class linearRegression():
 		if learning_const == 0:
 			learning_const = 0.5
 
-		if info == True:
-			print("learning_slope : {} -- learning_const : {}".format(learning_slope, learning_const))
+		print("learning_slope : {} -- learning_const : {}".format(learning_slope, learning_const))
 
 		Y_pred = slope * X + const
 		tmp_s = sum(X * (Y_pred - Y)) / size
 		tmp_c = sum(Y_pred - Y) / size
-		while (max_step == 0 and (abs(tmp_c) > precision or abs(tmp_s) > precision)) or step < max_step:
+		while (iteration == 0 and (abs(tmp_c) > precision or abs(tmp_s) > precision)) or step < iteration:
 			step += 1
 			self.__evol_slope.append(slope)
 			self.__evol_const.append(const)
@@ -101,7 +125,7 @@ class linearRegression():
 			slope -= learning_slope * tmp_s
 			const -= learning_const * tmp_c
 			if (slope == float('Inf') or const  == float('Inf')):
-				print("Error : inf value, step : {}".format(step))
+				print("Error : inf value, iteration : {}".format(step))
 				return
 			if info == True:
 				print ("{} : slope = {}\nconst = {}\n".format(step, slope, const))
@@ -116,14 +140,19 @@ class linearRegression():
 
 km = 200000		
 lr = linearRegression()
-lr.data_info()
-# print("data :\n", lr.data())
+# lr.data_info()
+print(lr.training.__doc__)
+# print("data :\n", lr.data_info())
 # print("estimate price for {} = {}".format(km, lr.estimatePrice(km)))
-lr.training()
+# lr.training(iteration = 10)
 # print("estimate price for {} = {}".format(km, lr.estimatePrice(km)))
-lr.display()
-lr.display(True)
-lr.display()
+# lr.training(iteration = 100)
+# print("estimate price for {} = {}".format(km, lr.estimatePrice(km)))
+# lr.training()
+# lr.training(precision = 0.005)
+# lr.display()
+# lr.display(True)
+# lr.display_value(km)
 
 # tmp_s = 0.007125317458163946
 # tmp_c = -0.3634002782132524
